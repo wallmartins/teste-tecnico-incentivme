903840f0bc4a3446f1f9a5332c2f510ff21fbc44
wallmartins <wall_martins_@hotmail.com>
2021-06-04 21:18:05 -0300

    feat(fix): fix request api to https url problem

--- src/Components/PagesComponents/FormPages/FormAddress.jsx
+++ src/Components/PagesComponents/FormPages/FormAddress.jsx
@@ -13,7 +13,7 @@ const FormAddress = () => {
 
   async function searchAdress() {
     const cep = userCEP?.toString().replace(/[^\d]+/g, '');
-    const response = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const json = await response.json();
     setAddress(json);
   }
